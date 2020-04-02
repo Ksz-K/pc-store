0f58d78617be96cb5b812d11c1469a7e20010938
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-04-02 12:06:11 +0200

    top anim done

--- src/components/Curtain.js
+++ src/components/Curtain.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { Timeline } from "gsap/gsap-core";
 
+let tl = new Timeline({ delay: 0.77 });
 const Curtain = () => {
+  useEffect(() => {
+    tl.to(".left", { duration: 1, visibility: "visible" })
+      .to(".middle", { duration: 1, visibility: "visible" })
+      .to(".right", { duration: 1, visibility: "visible" })
+      .to(".curtain", { duration: 0.1, backgroundColor: "rgba(255,255,255,0" })
+      .to(".left", { duration: 1, height: 0, ease: "bounce" }, "left")
+      .to(".left h1", { duration: 0.5, opacity: 0 }, "left+=0.2")
+      .to(".middle", { duration: 1, height: 0, ease: "bounce" }, "middle")
+      .to(".middle", { duration: 0.4, opacity: 0 }, "middle+=0.3")
+      .to(".right", { duration: 1, height: 0, ease: "bounce" }, "right")
+      .to(".right", { duration: 0.3, opacity: 0 }, "right+=0.4");
+  });
   return (
     <div className="curtain">
       <div className="top">
@@ -15,8 +30,8 @@ const Curtain = () => {
         </div>
       </div>
       <div className="bottom">
-        <div className="left">BL</div>
-        <div className="right">BR</div>
+        <div className="first">BL</div>
+        <div className="second">BR</div>
       </div>
     </div>
   );
