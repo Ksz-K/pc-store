60509788d76eadc286948575362108cc910fef85
Krzysztof Szachna-Krause <kszk@gmx.us>
2020-04-16 11:57:54 +0200

    inks done

--- src/pages/Consumables.js
+++ src/pages/Consumables.js
@@ -1,7 +1,121 @@
-import React from "react";
+import React, { Fragment, useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { Timeline } from "gsap/gsap-core";
+
+gsap.registerPlugin(Timeline);
 
 const Consumables = () => {
-  return <div>tusze</div>;
+  function createHover(cardWrapper) {
+    let tl = new Timeline({ paused: true, reversed: true });
+    tl.to(cardWrapper, {
+      duration: 1.2,
+      rotationY: 180,
+      ease: "Back.easeOut",
+    }).to(cardWrapper, {
+      duration: 1.2,
+      rotationY: 0,
+      ease: "Back.easeOut",
+    });
+    tl.play();
+  }
+
+  useEffect(() => {
+    gsap.to(".menu", { display: "flex" });
+    gsap.set(".cardWrapper", { perspective: 800 });
+    gsap.set(".card", { transformStyle: "preserve-3d" });
+    gsap.set(".back", { rotationY: -180 });
+    gsap.set([".back", ".front"], { backfaceVisibility: "hidden" });
+  }, []);
+
+  return (
+    <div className="inks">
+      <div className="cardWrapper d-none d-sm-block">
+        <div
+          className="card"
+          onMouseEnter={(e) => createHover(e.currentTarget)}
+        >
+          <div className="cardFace front">
+            <h1>Tusze</h1>
+            <h1>Tonery</h1>
+            <h1>Papier</h1>
+          </div>
+          <div className="cardFace back">
+            <img src="./img/inks.png" />
+          </div>
+        </div>
+      </div>
+      <div className="cardWrapper">
+        <div
+          className="card"
+          onMouseEnter={(e) => createHover(e.currentTarget)}
+        >
+          <div className="cardFace front">
+            <h1>HP Inc.</h1>
+            <p>Pełny wybór materiałów eksploatacyjnych</p>
+          </div>
+          <div className="cardFace back">
+            <img src="./img/hp.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="cardWrapper">
+        <div
+          className="card"
+          onMouseEnter={(e) => createHover(e.currentTarget)}
+        >
+          <div className="cardFace front">
+            <h1>Lexmark</h1>
+            <p>Tusze i zamienniki</p>
+          </div>
+          <div className="cardFace back">
+            <img src="./img/lexmark.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="cardWrapper">
+        <div
+          className="card"
+          onMouseEnter={(e) => createHover(e.currentTarget)}
+        >
+          <div className="cardFace front">
+            <h1>Canon</h1>
+            <p>Pełna oferta</p>
+          </div>
+          <div className="cardFace back">
+            <img src="./img/canon.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="cardWrapper">
+        <div
+          className="card"
+          onMouseEnter={(e) => createHover(e.currentTarget)}
+        >
+          <div className="cardFace front">
+            <h1>Brother</h1>
+            <p>Tonery</p>
+          </div>
+          <div className="cardFace back">
+            <img src="./img/brother.svg" />
+          </div>
+        </div>
+      </div>
+      <div className="cardWrapper">
+        <div
+          className="card"
+          onMouseEnter={(e) => createHover(e.currentTarget)}
+        >
+          <div className="cardFace front">
+            <h1>XEROX</h1>
+            <p>Tonery, tusze i papier</p>
+          </div>
+          <div className="cardFace back">
+            <img src="./img/xerox.svg" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Consumables;
